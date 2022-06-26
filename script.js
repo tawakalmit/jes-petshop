d01e384ffcfdc4ae3728883f1af46f86bab2c5ba
tawakalmit <tawakal.mit@gmail.com>
2022-06-26 15:48:45 +0700

    onscroll animation

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ menuToggle.addEventListener('click',function(){
 close.addEventListener('click', function() {
     sidebar.classList.toggle('active');
 })
+
+// onscroll reveal animation
+    // tangkap event ketika windows sedang di scroll
+    window.addEventListener('scroll', reveal );
+
+    // fungsi reveal
+    
+    function reveal(){
+      // tangkap DOM reveal
+      const reveals = document.querySelectorAll('.reveal');
+
+      // lakukan perulangan
+      for (let i = 0; i < reveals.length; i++){
+        // tangkap DOM
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 50;
+
+        // masukkan logic
+        if (revealTop < windowHeight - revealPoint){
+          reveals[i].classList.add('active');
+        } else {
+          reveals[i].classList.remove('active');
+        }
+      }
+    }
